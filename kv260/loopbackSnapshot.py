b25e2f13f8ffe5c551c9520823775ad05a5295f0
drkaputa <dskiee@rit.edu>
2026-02-14 10:39:48 -0500

    updated the loopbackSnapshot.py file to allow for variable image resolution.

---
+++ kv260/loopbackSnapshot.py
@@ -0,0 +1,65 @@
+# Dr. Kaputa
+# performs a loopback test for both feethrough and processing channels
+# need to have a .bin file loaded into the FPGA such as the kv260vdma project
+
+# https://github.com/ravvenlabs/vivado-vdma-project
+
+import cv2
+import numpy as np
+import time
+import mmap
+import struct
+import sys, random
+import ctypes
+import copy
+from frameGrabber import ImageFeedthrough
+from frameGrabber import ImageProcessing
+from frameGrabber import ImageWriter
+
+width = 1080
+height = 720
+depth = 8
+
+camProcessed = ImageProcessing(width,height,depth)
+camFeedthrough = ImageFeedthrough(width,height,depth)
+camWriter = ImageWriter(width,height,depth)
+
+time.sleep(1)
+print("writing frame to FPGA")
+# generate testing image
+data = np.zeros((height,width,depth), dtype=np.uint8)
+data[:,100,:] = 250 
+data[:,980,:] = 250 
+data[100,:,:] = 250 
+data[620,:,:] = 250 
+camWriter.setFrame(data)
+time.sleep(1)
+camWriter.setFrame(data)
+time.sleep(1)
+camWriter.setFrame(data)
+time.sleep(1)
+camWriter.setFrame(data)
+
+frameLeft,frameRight = camFeedthrough.getStereoRGB()    
+time.sleep(1)
+frameLeft,frameRight = camFeedthrough.getStereoRGB()    
+time.sleep(1)
+frameLeft,frameRight = camFeedthrough.getStereoRGB()    
+cv2.imwrite("leftFeedthrough.jpg", frameLeft)
+cv2.imwrite("rightFeedthrough.jpg", frameRight)
+
+camWriter.setFrame(data)
+time.sleep(1)
+camWriter.setFrame(data)
+time.sleep(1)
+camWriter.setFrame(data)
+time.sleep(1)
+camWriter.setFrame(data)
+frameLeft,frameRight = camProcessed.getStereoRGB()    
+time.sleep(1)
+frameLeft,frameRight = camProcessed.getStereoRGB()   
+time.sleep(1) 
+frameLeft,frameRight = camProcessed.getStereoRGB()  
+time.sleep(1)  
+cv2.imwrite("leftProcessing.jpg", frameLeft)
+cv2.imwrite("rightProcessing.jpg", frameRight)
